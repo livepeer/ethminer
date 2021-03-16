0fce3644d4b7e48ddf52a4d693fdbd86ed5f2085
Jai Luthra <me@jailuthra.in>
2021-03-17 02:13:32 +0530

    start.sh: Support CUDA MPS and other flags

--- start.sh
+++ start.sh
@@ -5,6 +5,9 @@ set -e
 # Cleanup ethminer when the script exits
 trap cleanup EXIT
 
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
+
 cleanup() {
     if [ -n "$ethminer_pid" ] && ps -p $ethminer_pid > /dev/null; then
         # Signal ethminer to exit
@@ -12,14 +15,24 @@ cleanup() {
     fi
 }
 
+start_cuda_mps_daemon() {
+    echo "Starting CUDA MPS in the background..."
+    export CUDA_VISIBLE_DEVICES=$NVIDIA_DEVICES
+    nvidia-cuda-mps-control -d &
+}
+
 start_ethminer() {
     echo "Starting ethminer in the background..."
-    ethminer -U -P $POOL_ADDR & 
+    ethminer -U -P $POOL_ADDR --cuda-streams $LP_CUDA_STREAMS \
+        --cuda-block-size $LP_CUDA_BLOCKSIZE --cuda-grid-size $LP_CUDA_GRIDSIZE \
+        --cuda-devices $NVIDIA_DEVICES &
     ethminer_pid=$!
 }
 
-start_ethminer
+start_cuda_mps_daemon
+sleep 1
 
+start_ethminer
 sleep 1
 
 exec livepeer "$@"
